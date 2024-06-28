338d7e13ad68863a3894a92f34477f739646248f
aayush_shah002 <iakashshah007@gmail.com>
2024-06-28 14:55:41 +0545

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -1,11 +1,9 @@
 import {cart, removeFromCart,updateDeliveryOption} from '../../data/cart.js';
 import { products,getProduct } from '../../data/products.js';
 import formatCurrency from '../utils/money.js';
-import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
-import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOption.js'
-
-hello(); 
+import {deliveryOptions, getDeliveryOption} from '../../data/deliveryOption.js';
+import { renderPaymentSummary } from './paymentSummary.js';
 
 export function renderOrderSummary(){
 
@@ -112,6 +110,8 @@ export function renderOrderSummary(){
             removeFromCart(productId);
             const container = document.querySelector(`.js-cart-item-container-${productId}`);
             container.remove();
+
+            renderPaymentSummary();
         })
     });
 
@@ -120,6 +120,7 @@ export function renderOrderSummary(){
             const {productId,deliveryOptionId} = element.dataset; 
             updateDeliveryOption(productId,deliveryOptionId);
             renderOrderSummary();
+            renderPaymentSummary();
         });
     });
 }
